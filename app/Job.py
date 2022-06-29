f651c4f30df997702a0b50d18d981caca172dbb9
Vornei Augusto Grella <vagrella@unicamp.br>
2022-06-29 19:04:13 -0300

    Escrevendo a lista de jobs, usando laço for, usando pandas e melhorias no arquivo jobs.json, adicionando a primeira linha de Janela de execusão e o período

--- app/Job.py
+++ app/Job.py
@@ -2,6 +2,8 @@
 
 #Nomes dos Campos
 class Campo:
+    janela_execucao = 'Janela de execução'
+    lista = 'lista'
     id = 'ID'
     descricao = 'Descrição'
     data_maxima_conclusao = 'Data Máxima de conclusão'
@@ -10,6 +12,7 @@ class Campo:
 #Informações
 class Info:
     def __init__(self, Info):
+        self.janela_execucao = Info.janela_execucao
         self.id = Info.id
         self.descricao = Info.descricao
         self.data_maxima_conclusao = Info.data_maxima_conclusao
--- app/retorna_jobs.py
+++ app/retorna_jobs.py
@@ -11,13 +11,19 @@ Arquivo Json com massa de dados criado com sucesso!
 >>> ler_jobs()
 Lendo Jobs...
 Dados lidos:
-[{'ID': 1, 'Descrição': 'Importação de arquivos de fundos', 'Data Máxima de conclusão': '2019-11-10 12:00:00', 'Tempo estimado': '2 horas'}, {'ID': 2, 'Descrição': 'Importação de dados da Base Legada', 'Data Máxima de conclusão': '2019-11-11 12:00:00', 'Tempo estimado': '4 horas'}, {'ID': 3, 'Descrição': 'Importação de dados de integração', 'Data Máxima de conclusão': '2019-11-11 08:00:00', 'Tempo estimado': '6 horas'}]
+[{'Janela de execução': '2019-11-10 09:00:00 até 2019-11-11 12:00:00', 'lista': [{'ID': 1, 'Descrição': 'Importação de arquivos de fundos', 'Data Máxima de conclusão': '2019-11-10 12:00:00', 'Tempo estimado': '2 horas'}, {'ID': 2, 'Descrição': 'Importação de dados da Base Legada', 'Data Máxima de conclusão': '2019-11-11 12:00:00', 'Tempo estimado': '4 horas'}, {'ID': 3, 'Descrição': 'Importação de dados de integração', 'Data Máxima de conclusão': '2019-11-11 08:00:00', 'Tempo estimado': '6 horas'}]}]
 
 
 >>> listar_jobs()
 Listando Jobs:
 Lendo Jobs...
 Dados lidos:
+[
+1,
+2,
+3,
+]
+
 
 '''
 import config
@@ -29,26 +35,29 @@ import pandas as pd
 Criar o arquivo com o Job
 '''
 def criar_job():
-    j = [
-        {
-            job.Campo.id: 1,
-            job.Campo.descricao: "Importação de arquivos de fundos",
-            job.Campo.data_maxima_conclusao: '2019-11-10 12:00:00',
-            job.Campo.tempo_estimado: '2 horas',
-        },
-        {
-            job.Campo.id: 2,
-            job.Campo.descricao: "Importação de dados da Base Legada",
-            job.Campo.data_maxima_conclusao: '2019-11-11 12:00:00',
-            job.Campo.tempo_estimado: '4 horas',
-        },
-        {
-            job.Campo.id: 3,
-            job.Campo.descricao: "Importação de dados de integração",
-            job.Campo.data_maxima_conclusao: '2019-11-11 08:00:00',
-            job.Campo.tempo_estimado: '6 horas',
-        }
-    ]
+    j = [{
+        job.Campo.janela_execucao: '2019-11-10 09:00:00 até 2019-11-11 12:00:00',
+        job.Campo.lista: [
+            {
+                job.Campo.id: 1,
+                job.Campo.descricao: "Importação de arquivos de fundos",
+                job.Campo.data_maxima_conclusao: '2019-11-10 12:00:00',
+                job.Campo.tempo_estimado: '2 horas',
+            },
+            {
+                job.Campo.id: 2,
+                job.Campo.descricao: "Importação de dados da Base Legada",
+                job.Campo.data_maxima_conclusao: '2019-11-11 12:00:00',
+                job.Campo.tempo_estimado: '4 horas',
+            },
+            {
+                job.Campo.id: 3,
+                job.Campo.descricao: "Importação de dados de integração",
+                job.Campo.data_maxima_conclusao: '2019-11-11 08:00:00',
+                job.Campo.tempo_estimado: '6 horas',
+            }
+        ],
+    }]
     #with open('./data/jobs.json', 'w') as arquivo:
     with open(config.arq_jobs, 'w') as arquivo:
         json.dump(j, arquivo, indent=config.indenta)
@@ -66,9 +75,32 @@ def ler_jobs():
     print('Dados lidos:')
     return jobs
 
+'''
+Listar os IDs dos Jobs
+'''
 def listar_jobs():
     print('Listando Jobs:')
+    # 1) Cada array do conjunto representa uma lista de Jobs a serem executados em sequência;
     lista_jobs = ler_jobs()
+    print('[')
+
+    # Maneira Inicial laço
+    for linha in lista_jobs:
+        #print(linha)
+        for linha_job in linha[job.Campo.lista]:
+            print(str(linha_job[job.Campo.id])+',')
+
+    # Esta com um erro: ValueError: Invalid file path or buffer object type: <class 'list'>
+    # Utilizar o pandas, porém não achei a melhor maneira de orientação para o tipo de arquivo Json inicialmente
+    # usando o read_json em que  o pandas pode ler alguns tipos
+    # json padrões orientado por: 'records', 'split', 'columns', 'values'
+    #pd.read_json(lista_jobs, orient='records')
+
+    #Usando json_normalize
+    pd.json_normalize(lista_jobs, record_path=[job.Campo.lista])
+
+    print(']')
+
 
 
 if __name__ == "__main__":
