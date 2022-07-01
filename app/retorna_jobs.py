84181397492afb28895f9e11135a7a00b6f5c0cf
Vornei Augusto Grella <vagrella@unicamp.br>
2022-07-01 09:47:16 -0300

    Melhorias nas configurações e testes

--- app/retorna_jobs.py
+++ app/retorna_jobs.py
@@ -8,23 +8,19 @@ doctests:
 >>> criar_job()
 Arquivo Json com massa de dados salvo com sucesso!
 
->>> ler_jobs()
-Lendo Jobs...
-Dados lidos:
-[{'Janela de execução': '2019-11-10 09:00:00 até 2019-11-11 12:00:00', 'lista': [{'ID': 1, 'Descrição': 'Importação de arquivos de fundos', 'Data Máxima de conclusão': '2019-11-10 12:00:00', 'Tempo estimado': '2 horas'}, {'ID': 2, 'Descrição': 'Importação de dados da Base Legada', 'Data Máxima de conclusão': '2019-11-11 12:00:00', 'Tempo estimado': '4 horas'}, {'ID': 3, 'Descrição': 'Importação de dados de integração', 'Data Máxima de conclusão': '2019-11-11 08:00:00', 'Tempo estimado': '6 horas'}]}]
+>>> ler_jobs(None)
+O Caminho do arquivo deve ser do tipo string!
 
-
->>> listar_jobs()
+>>> import app.config as cg
+>>> listar_jobs(cg.arq_jobs_teste)
 Listando Jobs:
 Lendo Jobs...
-Dados lidos:
 [
 ]
 
-
 '''
-import app.config as config
-import app.model.Job as Job
+import config as config
+import model.Job as Job
 
 import json
 #import pandas as pd
@@ -60,7 +56,7 @@ def criar_job(j = [], arq_jobs = ''):
                 }
             ],
         }]
-    #with open('./data/jobs.json', 'w') as arquivo:
+
     with open(arq_jobs, 'w') as arquivo:
         json.dump(j, arquivo, indent=config.indenta)
     print('Arquivo Json com massa de dados salvo com sucesso!')
@@ -68,18 +64,15 @@ def criar_job(j = [], arq_jobs = ''):
 '''
 Ler o arquivo com os Jobs retornando o dicionário de jobs do arquivo Json
 '''
-def ler_jobs(arq_jobs = ''):
-    print('Lendo Jobs...')
-    #with open('./data/jobs.json') as arquivo:
-
-    if not arq_jobs:
-        arq_jobs = config.arq_jobs_teste
-
-    with open(arq_jobs) as arquivo:
-        #dicionario de dados contendo os jobs
-        jobs = json.load(arquivo)
-    print('Dados lidos:')
-    return jobs
+def ler_jobs(arq_jobs):
+    if arq_jobs is None or not isinstance(arq_jobs, str):
+        print("O Caminho do arquivo deve ser do tipo string!")
+    else:
+        print('Lendo Jobs...')
+        with open(arq_jobs) as arquivo:
+            #dicionario de dados contendo os jobs
+            jobs = json.load(arquivo)
+        return jobs
 
 '''
 Listar os IDs dos Jobs:
@@ -88,16 +81,21 @@ Listar os IDs dos Jobs:
 3) Deve ser respeitada a data máxima de conclusão do Job;
 4) Todos os Jobs devem ser executados dentro da janela de execução (data início e fim).
 '''
-def listar_jobs(arq_jobs = ''):
-    print('Listando Jobs:')
-    if not arq_jobs:
-        arq_jobs = config.arq_jobs_teste
+def listar_jobs(arq_jobs):
 
+    if arq_jobs is None or not isinstance(arq_jobs, str):
+        raise AttributeError('O Caminho do arquivo deve ser do tipo string!')
+
+    print('Listando Jobs:')
     # 1) Cada array do conjunto representa uma lista de Jobs a serem executados em sequência;
-    dicionario_janelas = ler_jobs(arq_jobs)
+    dic_janelas = ler_jobs(arq_jobs)
+
+    if len(dic_janelas) == 0:
+        return None
+
     print('[')
     # Janelas de execuções
-    for janela in dicionario_janelas:
+    for janela in dic_janelas:
         # 4) Todos os Jobs devem ser executados dentro da janela de execução (data início e fim).
         janela_execucao = janela[Job.campo.janela_execucao].split(' até ', 1)
         data_inicio = config.dt.datetime.strptime(janela_execucao[0], config.arg_data_hora)
@@ -105,7 +103,8 @@ def listar_jobs(arq_jobs = ''):
 
         if data_inicio <= config.data_hora_atual <= data_fim:
             # 2) Cada array deve conter jobs que sejam executados em, no máximo, 8h;
-            # Usando json_normalize
+            # TODO máximo de 8 h:
+            #  Usando json_normalize para
             #lista_jobs_max_8h = pd.json_normalize(janela, record_path=[Job.campo.lista])
             lista_jobs_max_8h = janela[Job.campo.lista]
             print('[')
@@ -124,23 +123,8 @@ def listar_jobs(arq_jobs = ''):
 
 if __name__ == "__main__":
     # Para Executar os Testes automatizados
-    import doctest
-    doctest.testmod()
-
-    # Realizar etapas de execução
-    criar_job(arq_jobs=config.arq_jobs)
-
-    # Preparar: alterar o json para data atual
-    dic_jobs = ler_jobs(config.arq_jobs)
-    # Colocar a data fim da Janela para 5 dias
-    data_futuro = config.data_atual + config.dt.timedelta(days=5)
-    dic_jobs[0][Job.campo.janela_execucao] = str(config.data_atual) + ' 00:00:00 até ' + str(data_futuro) + ' 00:00:00'
-
-    # Alterar o período do indice 1 [ID: 2]
-    dic_jobs[0][Job.campo.lista][1][Job.campo.data_maxima_conclusao] = str(config.data_atual) + ' 23:00:00'
-
-    # Salvar alterações no arquivo
-    criar_job(dic_jobs, arq_jobs=config.arq_jobs)
+    #import doctest
+    #doctest.testmod()
 
     # Rodar a aplicação
     listar_jobs(arq_jobs=config.arq_jobs)
--- app/test/test_retorna_jobs.py
+++ app/test/test_retorna_jobs.py
@@ -1,15 +1,33 @@
 # encoding: utf-8
 import unittest
 
+import app.config as config
+import app.model.Job as Job
 import app.retorna_jobs as rj
 
 class TestRetornaJobs(unittest.TestCase):
 
     def setUp(self):
+        rj.criar_job(arq_jobs=config.arq_jobs)
+
+        # Preparar: alterar o json para data atual
+        dic_jobs = rj.ler_jobs(config.arq_jobs)
+        # Colocar a data fim da Janela para 5 dias
+        data_futuro = config.data_atual + config.dt.timedelta(days=5)
+        dic_jobs[0][Job.campo.janela_execucao] = str(config.data_atual) + ' 00:00:00 até ' + str(
+            data_futuro) + ' 00:00:00'
+
+        # Alterar o período do indice 1 [ID: 2]
+        dic_jobs[0][Job.campo.lista][1][Job.campo.data_maxima_conclusao] = str(config.data_atual) + ' 23:00:00'
+
+        # Salvar alterações no arquivo
+        rj.criar_job(dic_jobs, arq_jobs=config.arq_jobs)
         self.dict_jobs = rj.ler_jobs()
 
     def test_lista_jobs_vazio(self):
-        rj.listar_jobs('')
+        with self.assertRaises(AttributeError):
+            rj.listar_jobs(None)
+
 
 if __name__ == '__main__':
     unittest.main()
