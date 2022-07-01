70bc1d1397ba159a8beae4315200ad1411e0aa36
Vornei Augusto Grella <vagrella@unicamp.br>
2022-07-01 11:08:00 -0300

    Melhorias nos Testes

--- test/test_retorna_jobs.py
+++ test/test_retorna_jobs.py
@@ -2,17 +2,16 @@
 import unittest
 
 import app.config as config
-import app.retorna_jobs as rj
-
 import app.model.Job as Job
+import app.retorna_jobs as rj
 
 class TestRetornaJobs(unittest.TestCase):
 
     def setUp(self):
-        rj.criar_job(arq_jobs=config.arq_jobs)
 
+        rj.criar_job()
         # Preparar: alterar o json para data atual
-        dic_jobs = rj.ler_jobs(config.arq_jobs)
+        dic_jobs = rj.ler_jobs(config.arq_jobs_teste)
         # Colocar a data fim da Janela para 5 dias
         data_futuro = config.data_atual + config.dt.timedelta(days=5)
         dic_jobs[0][Job.campo.janela_execucao] = str(config.data_atual) + ' 00:00:00 até ' + str(data_futuro) + ' 00:00:00'
@@ -21,13 +20,12 @@ class TestRetornaJobs(unittest.TestCase):
         dic_jobs[0][Job.campo.lista][1][Job.campo.data_maxima_conclusao] = str(config.data_atual) + ' 23:00:00'
 
         # Salvar alterações no arquivo
-        rj.criar_job(dic_jobs, arq_jobs=config.arq_jobs)
+        rj.criar_job(dic_jobs, arq_jobs=config.arq_jobs_teste)
 
 
-    def test_lista_jobs_vazio(self):
-        with self.assertRaises(AttributeError):
-            rj.listar_jobs(None)
-
+    # def test_lista_jobs_vazio(self):
+    #     with self.assertRaises(AttributeError):
+    #         rj.listar_jobs(None)
 
     def test_lista_jobs_ok(self):
         self.jobs = rj.listar_jobs(config.arq_jobs_teste)
